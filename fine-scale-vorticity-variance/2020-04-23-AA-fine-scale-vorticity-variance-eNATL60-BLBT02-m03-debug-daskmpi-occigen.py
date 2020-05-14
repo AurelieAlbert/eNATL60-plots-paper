46f386dd5881a6da5740fb4e15c8f48b8caa00ef
auraoupa <auraoupa@gmail.com>
2020-05-14 09:09:37 +0200

    from mac

--- fine-scale-vorticity-variance/2020-04-23-AA-fine-scale-vorticity-variance-eNATL60-BLBT02-m03-debug-daskmpi-occigen.py
+++ fine-scale-vorticity-variance/2020-04-23-AA-fine-scale-vorticity-variance-eNATL60-BLBT02-m03-debug-daskmpi-occigen.py
@@ -1,49 +1,14 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
 
 
-from dask_jobqueue import SLURMCluster 
-from dask.distributed import Client 
-  
-cluster = SLURMCluster(cores=28,name='make_profiles',walltime='01:30:00',job_extra=['--constraint=HSW24','--exclusive','--nodes=1'],memory='120GB') 
-cluster.scale(240)
-cluster
+from dask_mpi import initialize
+initialize()
+
+from distributed import Client
+client = Client()
 
 
-
-
-# In[ ]:
-
-
-from dask.distributed import Client
-client = Client(cluster)
-client
-
-
-# In[ ]:
-
-
-print(len(client.scheduler_info()["workers"]))
-
-
-# In[ ]:
-
-
-get_ipython().system(u'squeue -u albert7a')
-
-import time
-nb_workers = 0
-while True:
-    nb_workers = len(client.scheduler_info()["workers"])
-    if nb_workers >= 20:
-        break
-    time.sleep(1)
-print(nb_workers)
-
-
-# In[ ]:
+print('Currently working with '+str(len(client.scheduler_info()["workers"]))+' workers')
 
 
 ## path for mdules
@@ -62,32 +27,12 @@ import numpy.ma as ma
 import xarray as xr
 import time
 
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-
-import matplotlib.cm as mplcm
-seq_cmap = mplcm.Blues
-div_cmap = mplcm.seismic
-
-### quick plot
-import matplotlib.pyplot as plt
 
 import glob
 import os 
 
-get_ipython().magic(u'matplotlib inline')
-
-
-# In[ ]:
-
-
 config='eNATL60'
 case='BLBT02'
-
-
-# In[ ]:
-
 
 ## Dataset
 
@@ -95,9 +40,6 @@ gridfile='/store/CT1/hmg2840/lbrodeau/eNATL60/eNATL60-I/coordinates_eNATL60.nc'
 maskfile='/store/CT1/hmg2840/lbrodeau/eNATL60/eNATL60-I/mesh_mask_eNATL60_3.6.nc'
 meshhgrfile='/store/CT1/hmg2840/lbrodeau/eNATL60/eNATL60-I/mesh_mask_eNATL60_3.6.nc'
 meshzgrfile='/store/CT1/hmg2840/lbrodeau/eNATL60/eNATL60-I/mesh_mask_eNATL60_3.6.nc'
-
-
-# In[ ]:
 
 
 grid=xr.open_dataset(gridfile,chunks={'y':700,'x':1000})
@@ -111,9 +53,6 @@ e2v=mesh.e2v[0]
 ff=mesh.ff[0]
 
 
-# In[ ]:
-
-
 def filt(w):
     win_box2D = w.window
     win_box2D.set(window='hanning', cutoff=90, dim=['x', 'y'], n=[90, 90])
@@ -121,9 +60,6 @@ def filt(w):
     w_LS = win_box2D.convolve(weights=bw)
     w_SS=w-w_LS
     return w_SS
-
-
-# In[ ]:
 
 
 def curl(u,v,e1v,e2u,ff):
@@ -147,38 +83,10 @@ def curl(u,v,e1v,e2u,ff):
     return curl
 
 
-# In[ ]:
 
 
-def plot_fine_scale_variance(var,loncrs,latcrs,lon,lat,hpvarm,month,m,config,case):
-    ''' map of the averaged fine scale variance
-    '''
-    fig, ax = plt.subplots(1,1,figsize=(15,10))
-    ax = plt.subplot(111,projection=ccrs.PlateCarree(central_longitude=0))
-    ax.autoscale(tight=True)
-    cont=np.isnan(hpvarm)
-    gl = ax.gridlines(draw_labels=True, linestyle=':', color='black',
-                      alpha=0.5)
-    gl.xformatter = LONGITUDE_FORMATTER
-    gl.yformatter = LATITUDE_FORMATTER
-    gl.xlabel_style = {'size': 25, 'color': 'gray'}
-    gl.ylabel_style = {'size': 25, 'color': 'gray'}
-    
-    ax.tick_params('both',labelsize=25)
 
-    pcolor = ax.pcolormesh(loncrs,latcrs,ma.masked_invalid(var),cmap=seq_cmap,vmin=0,vmax=0.1,alpha=1)
-    ax.contour(lon,lat,cont,alpha=0.5,linewidth=0.000001,antialiased=True,colors='black')
-    cbar = plt.colorbar(pcolor,orientation='horizontal',pad=0.1)
-    cbar.ax.tick_params(labelsize=25)
-    cbar.set_label('Small scales surface vorticity variance in '+month+' for '+config+'-'+case,fontsize=15)
-    plt.savefig(config+'-'+case+'_m'+m+'_fine_scale_variance_vorticity.png')
-  
-
-
-# In[ ]:
-
-
-def plot_vorticity_variance(month):
+def compute_vorticity_variance(month):
     print('Start')
     datadir='/store/CT1/hmg2840/lbrodeau/'+str(config)+'/'+str(config)+'-'+str(case)+'*-S/'
     filesU=datadir+'*/'+str(config)+'-'+str(case)+'*_1h_*_gridU-2D_20??'+str(month)+'??-20??'+str(month)+'??.nc'
@@ -207,26 +115,21 @@ def plot_vorticity_variance(month):
     loncrs=crs.return_ravel(np.asarray(navlon2))
     latcrsm=np.mean(latcrs,axis=-3)
     loncrsm=np.mean(loncrs,axis=-3)
-    print('Plotting')
-    if month == '03':
-        print('March')
-        plot_fine_scale_variance(hpcurl2mcm,loncrsm, latcrsm,navlon,navlat,hpcurl2m,'March',month,config,case)
-    if month == '09':
-        print('September')
-        plot_fine_scale_variance(hpcurl2mcm,loncrsm, latcrsm,navlon,navlat,hpcurl2m,'September',month,config,case)
-        
-
-
-# In[ ]:
-
+    print('Saving to netcdf')
+    hpcurl2mcm_dataset=hpcurl2mcm.to_dataset(name='coarse-filt-curl') 
+    hpcurl2mcm_dataset['latcrsm']=latcrsm
+    hpcurl2mcm_dataset['loncrsm']=loncrsm
+    hpcurl2mcm_dataset['hpcurl2m']=hpcurl2m
+    try:
+      os.mkdir('/scratch/cnt0024/hmg2840/albert7a/'+config+'/'+config+'-'+case+'-S/CURL/') 
+    except OSError as error: 
+      print(error)
+    hpcurl2mcm_dataset.to_netcdf(path='/scratch/cnt0024/hmg2840/albert7a/'+config+'/'+config+'-'+case+'-S/CURL/'+config+'-'+case+'_m'+str(month)+'_coarse-filt-surf-curl.nc',,mode='w')
 
 month_list=['03']
 
 for month in month_list:
     plot_vorticity_variance(month)
-
-
-# In[ ]:
 
 
 
